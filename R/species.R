d036c7a791f819329101390ed5af0c429a331d84
Christian Che-Castaldo <chrischecastaldo@icloud.com>
2020-11-10 14:25:54 -0800

    adds citation url link

--- R/species.R
+++ R/species.R
@@ -12,6 +12,6 @@
 #' @source \url{http://www.penguinmap.com/}
 #'
 #' @section References:
-#' Humphries, G. R. W., R. Naveen, M. Schwaller, C. Che-Castaldo, P. McDowall, M. Schrimpf and H. J. Lynch. (2017). "Mapping Application for Penguin Populations and Projected Dynamics (MAPPPD): Data and tools for dynamic management and decision support". _Polar Record_ 53(2): 160-166, https://doi.org/10.1017/S0032247417000055
+#' Humphries, G. R. W., R. Naveen, M. Schwaller, C. Che-Castaldo, P. McDowall, M. Schrimpf and H. J. Lynch. (2017). "Mapping Application for Penguin Populations and Projected Dynamics (MAPPPD): Data and tools for dynamic management and decision support". _Polar Record_ 53(2): 160-166,  \url{https://doi.org/10.1017/S0032247417000055}
 #'
 "species"
