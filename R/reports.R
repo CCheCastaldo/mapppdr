376f76862133a19349783a571f7e046060a3e3c3
Christian Che-Castaldo <chrischecastaldo@icloud.com>
2020-11-10 14:24:49 -0800

    adds citation url link, changes paper to party

--- R/reports.R
+++ R/reports.R
@@ -8,7 +8,7 @@
 #'   \item{citekey}{unique identifier for each citation}
 #'   \item{title}{report title (may be title for specific chapter in a report)}
 #'   \item{institution}{institution that publishes report}
-#'   \item{type}{report type or name of third paper who prepared the report for the institution, if either is applicable}
+#'   \item{type}{report type or name of third party who prepared the report for the institution, if either is applicable}
 #'   \item{number}{unique report identifier as designated by the institution}
 #'   \item{city}{publisher city}
 #'   \item{country}{publisher country}
@@ -17,6 +17,6 @@
 #' @source \url{http://www.penguinmap.com/}
 #'
 #' @section References:
-#' Humphries, G. R. W., R. Naveen, M. Schwaller, C. Che-Castaldo, P. McDowall, M. Schrimpf and H. J. Lynch. (2017). "Mapping Application for Penguin Populations and Projected Dynamics (MAPPPD): Data and tools for dynamic management and decision support". _Polar Record_ 53(2): 160-166, https://doi.org/10.1017/S0032247417000055
+#' Humphries, G. R. W., R. Naveen, M. Schwaller, C. Che-Castaldo, P. McDowall, M. Schrimpf and H. J. Lynch. (2017). "Mapping Application for Penguin Populations and Projected Dynamics (MAPPPD): Data and tools for dynamic management and decision support". _Polar Record_ 53(2): 160-166,  \url{https://doi.org/10.1017/S0032247417000055}
 #'
 "reports"
