45572cb4221e4eb42d026f78905c801e57173684
Christian Che-Castaldo <chrischecastaldo@icloud.com>
2020-11-10 14:24:34 -0800

    adds citation url link, standardizes how we refer to MAPPPD with other man files

--- R/persons.R
+++ R/persons.R
@@ -1,7 +1,7 @@
 #' MAPPPD contributor bibliographic identifiers
 #'
 #' A dataset containing bibliographic identifiers for all references
-#' contained within the MAPPPD database.
+#' contained within MAPPPD.
 #'
 #' @format A data frame with 316 rows and 3 variables:
 #' \describe{
@@ -12,6 +12,6 @@
 #' @source \url{http://www.penguinmap.com/}
 #'
 #' @section References:
-#' Humphries, G. R. W., R. Naveen, M. Schwaller, C. Che-Castaldo, P. McDowall, M. Schrimpf and H. J. Lynch. (2017). "Mapping Application for Penguin Populations and Projected Dynamics (MAPPPD): Data and tools for dynamic management and decision support". _Polar Record_ 53(2): 160-166, https://doi.org/10.1017/S0032247417000055
+#' Humphries, G. R. W., R. Naveen, M. Schwaller, C. Che-Castaldo, P. McDowall, M. Schrimpf and H. J. Lynch. (2017). "Mapping Application for Penguin Populations and Projected Dynamics (MAPPPD): Data and tools for dynamic management and decision support". _Polar Record_ 53(2): 160-166,  \url{https://doi.org/10.1017/S0032247417000055}
 #'
 "persons"
