385de0bfe162403119a8712bdc58ada74aaf9d64
Christian Che-Castaldo <chrischecastaldo@icloud.com>
2020-11-10 14:25:45 -0800

    adds citation url link, clarifies these sites are within MAPPPD, adds more precise language re sf objects

--- R/sites_sf.R
+++ R/sites_sf.R
@@ -1,22 +1,23 @@
 #' Name and location of MAPPPD seabird breeding sites
 #'
-#' A dataset containing the name and location, stored as a simple feature, of all known Pygoscelis
-#' and Emperor penguin breeding sites south of 60 degrees S latitude and all Antarctic Site Inventory
-#' (ASI) penguin/seabird census sites.
+#' A dataset containing the name and location of all known Pygoscelis
+#' and Emperor penguin breeding sites south of 60 degrees S latitude
+#' and all Antarctic Site Inventory (ASI) penguin/seabird census sites
+#' contained within MAPPPD.
 #'
-#' @format A data frame with 748 rows and 5 variables:
+#' @format An sf data frame with 748 rows, 4 attribute variables, and 1 simple features geometry column:
 #' \describe{
 #'   \item{site_id}{unique identifier for each site}
 #'   \item{site_name}{site name and/or description}
 #'   \item{region}{site region as reported in the 2015 ASI Antarctic Peninsula Compendium (Naveen and Lynch, 2015) }
 #'   \item{ccamlr_id}{Commission for the Conservation of Antarctic Marine Living Resources (CCAMLR) subarea within which the site lies}
-#'   \item{geometry}{POINT geometry of site location (EPSG:4326)}
+#'   \item{geometry}{simple features POINT geometries of all site location (EPSG:4326)}
 #' }
 #' @source \url{http://www.penguinmap.com/}
 #' @source \url{https://oceanites.org/research-portal/antarctic-site-inventory/asi-census-sites/}
 #'
 #' @section References:
-#' Humphries, G. R. W., R. Naveen, M. Schwaller, C. Che-Castaldo, P. McDowall, M. Schrimpf and H. J. Lynch. (2017). "Mapping Application for Penguin Populations and Projected Dynamics (MAPPPD): Data and tools for dynamic management and decision support". _Polar Record_ 53(2): 160-166, https://doi.org/10.1017/S0032247417000055
+#' Humphries, G. R. W., R. Naveen, M. Schwaller, C. Che-Castaldo, P. McDowall, M. Schrimpf and H. J. Lynch. (2017). "Mapping Application for Penguin Populations and Projected Dynamics (MAPPPD): Data and tools for dynamic management and decision support". _Polar Record_ 53(2): 160-166,  \url{https://doi.org/10.1017/S0032247417000055}
 #'
 #' Naveen, R. and Lynch H. J. (2015). "Antarctic Peninsula Compendium" 3rd Edition, Chevy Chase, Maryland, USA.
 "sites_sf"
