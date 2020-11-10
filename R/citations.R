8d4e4b16e80115a6d222e4a8eafa5fb6d3bb640f
Christian Che-Castaldo <chrischecastaldo@icloud.com>
2020-11-10 14:21:06 -0800

    adds citation url link, describes allowed bibtype values

--- R/citations.R
+++ R/citations.R
@@ -6,12 +6,12 @@
 #' @format A data frame with 187 rows and 3 variables:
 #' \describe{
 #'   \item{citekey}{unique identifier for each citation}
-#'   \item{bibtype}{citation type}
+#'   \item{bibtype}{citation type, can be article (for journal articles), book, book chapter, conference paper, personal communication, report (for unpublished technical reports), or unpublished (for unpublished datasets, monographs, reports, supplements, theses, etc.). }
 #'   \item{year}{citation year}
 #' }
 #' @source \url{http://www.penguinmap.com/}
 #'
 #' @section References:
-#' Humphries, G. R. W., R. Naveen, M. Schwaller, C. Che-Castaldo, P. McDowall, M. Schrimpf and H. J. Lynch. (2017). "Mapping Application for Penguin Populations and Projected Dynamics (MAPPPD): Data and tools for dynamic management and decision support". _Polar Record_ 53(2): 160-166, https://doi.org/10.1017/S0032247417000055
+#' Humphries, G. R. W., R. Naveen, M. Schwaller, C. Che-Castaldo, P. McDowall, M. Schrimpf and H. J. Lynch. (2017). "Mapping Application for Penguin Populations and Projected Dynamics (MAPPPD): Data and tools for dynamic management and decision support". _Polar Record_ 53(2): 160-166,  \url{https://doi.org/10.1017/S0032247417000055}
 #'
 "citations"
