5687989b348b59aa164f43e2a80f0cd562325d5d
Christian Che-Castaldo <checastaldo@wisc.edu>
2026-07-02 20:56:40 -0500

    modernizes package-level documentation in `R/mapppdr.R` to use the `"_PACKAGE"` sentinel

--- R/mapppdr.R
+++ R/mapppdr.R
@@ -47,7 +47,4 @@
 #'
 #' Humphries, G.R.W., R. Naveen, M. Schwaller, C. Che-Castaldo, P. McDowall, M. Schrimpf and H.J. Lynch. (2017). "Mapping Application for Penguin Populations and Projected Dynamics (MAPPPD): Data and tools for dynamic management and decision support". _Polar Record_ 53(2): 160-166, \url{https://doi.org/10.1017/S0032247417000055}
 #'
-#' @docType package
-#' @name mapppdr
-
-NULL
+"_PACKAGE"
